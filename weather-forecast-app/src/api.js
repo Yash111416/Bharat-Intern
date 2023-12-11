494fa86d3dc1f423522431545eff779ae8982d3b
Yash Bishnoi <87704585+Yash111416@users.noreply.github.com>
2023-12-11 17:05:19 +0530

    Add files via upload

---
+++ weather-forecast-app/src/api.js
@@ -0,0 +1,14 @@
+export const url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
+export const getApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'd1412b6297msh412423adb2d08c8p140dcejsn152f1e41eac4',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = "1964dbb978dcc7a59b8602f61552ba0f";
